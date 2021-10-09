8ffffa4b507290e16e5b396ddc98723070cb5439
i92lomua <i92lomua@uco.es>
2021-10-09 18:16:26 +0200

    Factoria

--- src/data/ShowFactory.java
+++ src/data/ShowFactory.java
@@ -4,7 +4,7 @@ public class ShowFactory {
     
     public static ShowFactory getFactory(String type){
         if(type.equals("Puntual")){
-            return new PuntualShow();
+            return new PuntualShow(); 
         }else if(type.equals("Multiple")){
             return new MultipleShow();
         }else{
